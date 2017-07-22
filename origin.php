d6f4be9c407bfd76d32e0b86a341cd85e2321099
Pebri - Mobiwin Pc <junkuwieazy@gmail.com>
2017-07-23 01:20:08 +0700

    dd

--- origin.php
+++ origin.php
@@ -1,35 +1,40 @@
 <?php
 
 if (!empty($_GET)) {
-    //$username = $_GET['username'];
-    //$password = $_GET['password'];
     $msisdn = $_GET['msisdn'];
-    $trxId = $_GET['trxid'];
-    //$cost = $_GET['serviceId'];
     $sms = strtolower($_GET['sms']);
-    //$shortName = $_GET['shortname'];
-    $date = new DateTime();
-    $now = date_format($date, 'Y-m-d h:i:s');
+    $trxId = $_GET['trxid'];
+    $date = $_GET['trxdate'];
+    $shortcode = $_GET['shortcode'];
+    $now = date('Y-m-d H-i-s');
     $random = rand(1, 999999999);
 
-    $idMsisdn = '62' . substr($msisdn, 1);
+    if(substr($msisdn, 0,2) == '62') {
+      $idMsisdn = $msisdn;
+    } else {
+      $idMsisdn = '62' . substr($msisdn, 1);
+    }
 
     include_once 'db.php';
 
-    //$host = 'http://122.248.32.27:3000/mo/xl?';
-    $host = 'http://localhost:3000/mo/xl?';
+    $host = 'http://localhost/sms-engine-php-mysql-1/mo/xl?';
+    //$host = 'http://122.248.32.27:3008/mo/xl?';
 
-    $options = array("msisdn" => $idMsisdn, "sms" => $sms, "trxid" => $trxId, "trxdate" => $now, "shortcode" => 912345);
+    $options = array("msisdn" => $idMsisdn, "sms" => $sms, "trxid" => $trxId, "trxdate" => $date, "shortcode" => $shortcode);
     $host .= http_build_query($options, '', '&');
-    $myData = file_get_contents("$host");
 
-    if ($myData == 'MO OK') {
+    $opts = array('http'=>array('header' => "User-Agent:Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1\r\n"));
+    $context = stream_context_create($opts);
+    $myData = file_get_contents($host,false,$context);
+
+    if ($myData == 'ok') {
         $save = mysqli_query($con, "INSERT INTO inbox (msisdn,sms,date_cr,stat) VALUES('$idMsisdn','$sms','$now','origin')");
 
         if ($save) {
             echo $now . ' MO Engine ready ' . $random;
         }
     } else {
+      //echo $host;
         echo $now . ' MO Engine not ready ' . $random;
     }
 }
--- push.php
+++ push.php
@@ -2,39 +2,65 @@
 
 if (!empty($_GET)) {
 
-    if ($_GET['username'] == 'adminDB' && $_GET['password'] == 'passwordDb') {
+  if ($_GET['username'] == 'adminDB' && $_GET['password'] == 'passwordDb') {
 
-        $msisdn = $_GET['msisdn'];
-        $trxId = $_GET['trxid'];
-        $cost = $_GET['serviceId'];
-        $sms = strtolower($_GET['sms']);
-        $shortName = $_GET['shortname'];
-        $date = new DateTime();
-        $now = date_format($date, 'Y-m-d h:i:s');
-        $random = rand(1, 999999999);
+    $msisdn = $_GET['msisdn'];
+    $trxId = $_GET['trxid'];
+    $cost = $_GET['serviceId'];
+    $sms = strtolower($_GET['sms']);
+    $shortName = $_GET['shortname'];
+    $now = date('Y-m-d H-i-s');
+    $random = rand(1, 999999999);
 
-        $idMsisdn = '62' . substr($msisdn, 1);
-
-        include_once 'db.php';
-
-
-        $save = mysqli_query($con, "INSERT INTO inbox (msisdn,sms,date_cr,stat,biaya) VALUES('$idMsisdn','$sms','$now','reply','$cost')");
-        if ($save) {
-            if (empty($trxId)) {
-                echo 'ID-' . $random;
-                $trxId = 'ID-' . $random;
-            } else {
-                echo 'ok';
-            }
-
-            // SEND to DR
-            //$host = 'http://122.248.32.27:3000/dr/xl?';
-            $host = 'http://localhost:3000/dr/xl?';
-
-            $options = array("msisdn" => $idMsisdn, "trxid" => $trxId, "trxdate" => $now, "shortcode" => 912345, "stat" => 2);
-            $host .= http_build_query($options, '', '&');
-            $myData = file_get_contents("$host");
-        }
+    if(substr($msisdn, 0,2) == '62') {
+      $idMsisdn = $msisdn;
+    } else {
+      $idMsisdn = '62' . substr($msisdn, 1);
     }
+
+    $errMsg = 3;
+    if (empty($trxId)) {
+      $trxId = 'ID-' . $random;
+      //$output = array('error' => $errMsg, 'trxid' => 'ID-' . $random);
+      //echo http_build_query($output);
+
+      $newsXML = new SimpleXMLElement("<response></response>");
+      $newsIntro = $newsXML->addChild('error');
+      $newsIntro->addAttribute('code', $errMsg);
+      $newsIntro = $newsXML->addChild('trxid');
+      $newsIntro->addAttribute('id', 'ID-' . $random);
+      Header('Content-type: text/xml');
+      echo $newsXML->asXML();
+    } else {
+      //$output = array('error' => $errMsg = 0;, 'trxid' => $trxId);
+      //echo http_build_query($output);
+
+      $newsXML = new SimpleXMLElement("<response></response>");
+      $newsIntro = $newsXML->addChild('error');
+      $newsIntro->addAttribute('code', $errMsg);
+      $newsIntro = $newsXML->addChild('trxid');
+      $newsIntro->addAttribute('id', $trxId);
+      Header('Content-type: text/xml');
+      echo $newsXML->asXML();
+    }
+
+    include_once 'db.php';
+
+    // SEND to DR
+    $host = 'http://localhost/sms-engine-php-mysql-1/dr/xl?';
+    //$host = 'http://122.248.32.27:3000/dr/xl?';
+
+    $options = array("msisdn" => $idMsisdn, "trxid" => $trxId, "trxdate" => $now, "shortcode" => 912345, "stat" => $errMsg);
+
+    $host .= http_build_query($options, '', '&');
+
+    $opts = array('http'=>array('header' => "User-Agent:Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1\r\n"));
+    $context = stream_context_create($opts);
+    $myData = file_get_contents($host,false,$context);
+
+    if ($myData == 'ok') {
+      $save = mysqli_query($con, "INSERT INTO inbox (msisdn,sms,date_cr,stat,biaya) VALUES('$idMsisdn','$sms','$now','reply','$cost')");
+    }
+  }
 }
 ?>
